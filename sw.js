f088328490e091226307cf94e224c652bf482329
juanocampo7-arch <juan.ocampo7@utp.edu.co>
2026-05-07 08:45:16 -0500

    Simplify fetch event listener in sw.js
    Removed comments from fetch event listener in service worker.

--- sw.js
+++ sw.js
@@ -1,8 +1,4 @@
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Instalado');
 });
-
-self.addEventListener('fetch', (e) => {
-  // Este archivo se deja básico. 
-  // Solo con existir, Chrome habilita la opción de "Instalar App".
-});
+self.addEventListener('fetch', (e) => {});
